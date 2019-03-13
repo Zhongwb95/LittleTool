d5dc57e06089a6e367802eca9a63f8be91c1d12a
钟文冰 <42909153+Zhongwb95@users.noreply.github.com>
2019-03-13 10:26:38 +0800

    Update find_and_package.py

--- fpkg/find_and_package.py
+++ fpkg/find_and_package.py
@@ -3,35 +3,39 @@ import sys
 import zipfile
 
 
-def make_zip(source_dir, output_filename, keys):
-    zipf = zipfile.ZipFile(output_filename, 'w')
-	listfile = 'filelist.txt'
-    fl = open(listfile, 'w')
-    pre_len = len(os.path.dirname(source_dir))
+def make_zip(src_dir='', output='', keys=[]):
+    src_dir = src_dir.rstrip(os.path.sep) # 干掉末尾的'/'或'\'
+    if not output:
+        index = src_dir.rfind(os.path.sep) 
+        output = '{}.zip'.format(src_dir[index+1:])# 以源路径最后的作为压缩文件名
+    zipf = zipfile.ZipFile(output, 'w')
+    listfile = 'filelist.txt'
+    fl = open(listfile, 'w') # 创建压缩文件的记录文件
+    pre_len = len(os.path.dirname(src_dir))
     flag = True
-    for root, dirnames, filenames in os.walk(source_dir):
+    for root, dirnames, filenames in os.walk(src_dir):
         for filename in filenames:
             for key in keys:
                 if filename.find(key) != -1:
                     flag = False
-                    fl.write(os.path.join(root, filename))
-                    fl.write('\r\n')
                     pathfile = os.path.join(root, filename)
                     arcname = pathfile[pre_len:].strip(os.path.sep)   #相对路径
                     zipf.write(pathfile, arcname)
+                    fl.write(arcname)
+                    fl.write('\r\n')
     fl.close()
     zipf.write(listfile)
     zipf.close()
     os.remove(listfile)
     if flag :
-        os.remove(output_filename)
+        os.remove(output)
         print('no same as {} file'.format(keys))
 
 if __name__ == '__main__':
     """ 使用方法 ：
-    python find_and_package.py path : path为要查找文件夹的路径
+    python package.py path : path为要查找文件夹的路径
     """
     if len(sys.argv)>1 and os.path.isdir(sys.argv[1]):
-        make_zip(sys.argv[1], 'SConscript.zip', ['SConscript', 'SConstruct'])
+        make_zip(src_dir=sys.argv[1], keys=['SConscript', 'SConstruct'])
     else:
         print('no dir to zip %s' % os.path.sep)
