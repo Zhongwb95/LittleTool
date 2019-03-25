8ef8f78300af11076cff24c14ddeb4739c5a9473
钟文冰 <42909153+Zhongwb95@users.noreply.github.com>
2019-03-25 18:04:56 +0800

    Update Deduplication.py

--- tool/Deduplication.py
+++ tool/Deduplication.py
@@ -7,3 +7,18 @@ class Tool:
     对列表中的元素去重 （python3 有效）
     """
         return list(set(obj_list))
+    
+    def make_triangle(x):
+    """
+    生成一个指定层数的杨辉三角 
+    """
+        Y_triangle = list()
+        for i in range(1,x+1):
+            num = list()
+            for j in range(1,i+1):
+                if j==1 or j==i:
+                    num.append(1)
+                else:
+                    num.append(Y_triangle[i-2][j-2]+Y_triangle[i-2][j-1])
+            Y_triangle.append(num)
+        return Y_triangle
