1aa4d40f6bdcf17bd3404ea3e52a5b4992cab2f8
钟文冰 <zhongwb_95@163.com>
2019-10-22 21:59:59 +0800

    整理仓库

--- fpkg/find_and_package.py
+++ fpkg/find_and_package.py
@@ -4,13 +4,14 @@ import zipfile
 
 
 def make_zip(src_dir='', output='', keys=[]):
-    src_dir = src_dir.rstrip(os.path.sep) # 干掉末尾的'/'或'\'
+
+    src_dir = src_dir.rstrip(os.path.sep)  # 干掉末尾的'/'或'\'
     if not output:
         index = src_dir.rfind(os.path.sep) 
-        output = '{}.zip'.format(src_dir[index+1:])# 以源路径最后的作为压缩文件名
+        output = '{}.zip'.format(src_dir[index+1:])  # 以源路径最后的作为压缩文件名
     zipf = zipfile.ZipFile(output, 'w')
-    listfile = 'filelist.txt'
-    fl = open(listfile, 'w') # 创建压缩文件的记录文件
+    list_file = 'filelist.txt'
+    fl = open(list_file, 'w')  # 创建压缩文件的记录文件
     pre_len = len(os.path.dirname(src_dir))
     flag = True
     for root, dirnames, filenames in os.walk(src_dir):
@@ -19,17 +20,18 @@ def make_zip(src_dir='', output='', keys=[]):
                 if filename.find(key) != -1:
                     flag = False
                     pathfile = os.path.join(root, filename)
-                    arcname = pathfile[pre_len:].strip(os.path.sep)   #相对路径
+                    arcname = pathfile[pre_len:].strip(os.path.sep)   # 相对路径
                     zipf.write(pathfile, arcname)
                     fl.write(arcname)
                     fl.write('\r\n')
     fl.close()
-    zipf.write(listfile)
+    zipf.write(list_file)
     zipf.close()
-    os.remove(listfile)
-    if flag :
+    os.remove(list_file)
+    if flag:
         os.remove(output)
         print('no same as {} file'.format(keys))
+
 
 if __name__ == '__main__':
     """ 使用方法 ：
@@ -40,7 +42,7 @@ if __name__ == '__main__':
         if os.path.isdir(src_dir):
             make_zip(src_dir=src_dir, keys=['SConscript', 'SConstruct'])
         else:
-            print('No such directory: %' % src_dir)
+            print('No such directory: %s' % src_dir)
     else:
         print('Not enough arguments')
 
--- tool/Tool.py
+++ tool/Tool.py
@@ -3,47 +3,48 @@ import os
 import re
 import random
 
+
 class Password(object):
-"""
-根据需求生成随机字符串(可用来产生随机密码)
-使用方法:
+    """
+    根据需求生成随机字符串(可用来产生随机密码)
+    使用方法:
     # 第一个参数为随机字符串长度,0为8到32随机长度; 
     # 第二个参数为字符串组成成分,如:{'d':digits, 'l':line, 'u':upper, 's':special, 'o':lower}
     password = Password(8, 'udlo') 
     print(password())
     # 未进行输入校验,若第二个参数长度(类型总数)大于随机字符串长度或输入不存在类型会抛出异常
-"""
+    """
     lower = 'abcdefghijklmnopqrstuvwxyz'
     upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     digits = '0123456789'
     line = '_'
     special = '!@#$%^&*()+~.-=|\\?/;:><,[]{}"\'`'
-    dict = {'d':digits, 'l':line, 'u':upper, 's':special, 'o':lower}
+    dict = {'d': digits, 'l': line, 'u': upper, 's': special, 'o': lower}
 
     def __init__(self, length, types='unl'):
-        if self.checkInput(length, types): raise Exception
+        if self.check_input(length, types): raise Exception
         self.length = length
         self.types = types
         self.characters = ''
         self.password = ''
 
-    def checkInput(self, length, types):
+    def check_input(self, length, types):
         types = list(set(list(types)))
         keys = self.dict.keys()
         if len(types) > length:
             return True
         return sum([0 if _ in keys else 1 for _ in types])
 
-    def getRandomChar(self):
+    def get_random_char(self):
         return self.characters[random.randint(0, len(self.characters)-1)]
 
-    def setPasswordBase(self):
+    def set_password_base(self):
         if not self.length or self.length == 'random':
-            self.length = random.randint(8,32)
+            self.length = random.randint(8, 32)
         for tp in self.types:
             self.characters += self.dict.get(tp, '')
 
-    def checkPassword(self):
+    def check_password(self):
         fl = self.dict.copy()
         for i,v in fl.items():
             fl[i] = 0
@@ -52,33 +53,33 @@ class Password(object):
         if sum(fl.values()) == len(self.types):
             return True
 
-    def getRandomPassword(self):
-        self.setPasswordBase()
-        while not self.checkPassword():
+    def get_random_password(self):
+        self.set_password_base()
+        while not self.check_password():
             self.password = ''
             for i in range(self.length):
-                self.password += self.getRandomChar()
+                self.password += self.get_random_char()
 
     def __call__(self):
-        self.getRandomPassword()
+        self.get_random_password()
         return self.password  
 
 
 def make_triangle(x):
-"""
-生成一个指定层数的杨辉三角列表
-如：x=5 时列表如下
-[   [1],
-   [1,1],
-  [1,2,1],
- [1,3,3,1],
-[1,4,6,4,1], ]
-"""
+    """
+    生成一个指定层数的杨辉三角列表
+    如：x=5 时列表如下
+    [   [1],
+       [1,1],
+      [1,2,1],
+     [1,3,3,1],
+    [1,4,6,4,1], ]
+    """
     Y_triangle = list()
-    for i in range(1,x+1):
+    for i in range(1, x+1):
         num = list()
-        for j in range(1,i+1):
-            if j==1 or j==i:
+        for j in range(1, i+1):
+            if j == 1 or j == i:
                 num.append(1)
             else:
                 num.append(Y_triangle[i-2][j-2]+Y_triangle[i-2][j-1])
@@ -87,9 +88,9 @@ def make_triangle(x):
 
 
 def remove_note(file, single='//', pre='/*', suf='*/'):
-"""
-将文件中的注释处理掉,返回没有注释的文件名
-"""
+    """
+    将文件中的注释处理掉,返回没有注释的文件名
+    """
     def make_do(s_str):
         r_str = ''
         for ch in s_str:
@@ -105,16 +106,16 @@ def remove_note(file, single='//', pre='/*', suf='*/'):
     with open(file, 'rt') as sf:
         new_lines = re.sub(re_line, '', sf.read())
     filename = os.path.splitext(file)
-    out_file = '%s_unote%s'%(filename[0], filename[-1])
+    out_file = f'{filename[0]}_unote{filename[-1]})'
     with open(out_file, mode='w', encoding='utf-8') as rf:
         rf.writelines(new_lines)
     return out_file
 
 
-def isUtf8(file):
-"""
-判断文件是不是以utf-8编码
-"""
+def is_utf8(file):
+    """
+    判断文件是不是以utf-8编码
+    """
     with open(file, "rb") as f:
         byte = 1
         while byte:
@@ -122,11 +123,11 @@ def isUtf8(file):
             if byte:
                 data = '{:08b}'.format(ord(byte))
                 b = data.find('0')	    
-                if b==0:
+                if b == 0:
                     continue
-                if abs(b)==1:
+                if abs(b) == 1:
                     return False
                 for _ in range(b-1):
-                    if ord(f.read(1))&0xc0!=0x80:
+                    if ord(f.read(1)) & 0xc0 != 0x80:
                         return False
     return True
