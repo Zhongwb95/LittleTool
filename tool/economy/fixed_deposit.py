5a20bccde705c51aca9be31d6ae47c74a9a07689
钟文冰 <zhongwb_95@163.com>
2019-10-28 23:18:32 +0800

    。。。。穷啊

--- tool/economy/fixed_deposit.py
+++ tool/economy/fixed_deposit.py
@@ -1,10 +1,5 @@
 
 
-annual_interest_rates = 4.2/100
-deposit_time_month = 24
-monthly_deposit_money = 4000
-
-
 class AmountOfTime(object):
     time_type_multiple = {'year': 12 * 30 * 24 * 60, 'month': 30 * 24 * 60, 'day': 24 * 60, 'hour': 60, 'second': 1}
 
@@ -17,6 +12,7 @@ class AmountOfTime(object):
         return self.value/self.time_type_multiple.get(time_type)
 
 
+# 不考虑复利的情况
 def get_each_month_interest(interest_rates, time, money):
     interest_list = []
     interest_rates = interest_rates/12
@@ -25,8 +21,30 @@ def get_each_month_interest(interest_rates, time, money):
     return interest_list
 
 
+def get_compound_interest(compound_interest_cycle, interest_list):
+    interest = 0
+    num = int(len(interest_list)/compound_interest_cycle) * compound_interest_cycle
+    for i in range(num):
+        interest += interest_list[i]
+    return interest
+
+
+# 考虑复利的情况
+def get_each_month_interest_(interest_rates, time, money, cycle):
+    interest_list = []
+    interest_rates = interest_rates/12
+    for i in range(int(time.get_time('month'))):
+        interest = get_compound_interest(cycle, interest_list)
+        interest_list.append(interest_rates * (money * (i + 1) + interest))
+    return interest_list
+
+
 def get_interest(interest_rates, time, money):
     return sum(get_each_month_interest(interest_rates, time, money))
+
+
+def get_interest_(interest_rates, time, money, cycle):
+    return sum(get_each_month_interest_(interest_rates, time, money, cycle))
 
 
 def get_principal_and_interest(interest_rates, time, money):
@@ -34,11 +52,27 @@ def get_principal_and_interest(interest_rates, time, money):
     return get_interest(interest_rates, time, money) + principal
 
 
-if __name__ == '__main__':
-    # 不考虑复利的情况
-    moneys = get_interest(annual_interest_rates, AmountOfTime(360, 'month'), monthly_deposit_money)
-    print(moneys)
-    print(360 * 4000)
-    # 考虑复利的情况
-    # todo
+def get_principal_and_interest_(interest_rates, time, money, cycle):
+    principal = time.get_time('month') * money
+    return get_interest_(interest_rates, time, money, cycle) + principal
 
+
+if __name__ == '__main__':
+    annual_interest_rates = 4.5 / 100
+    month_ = 12
+    cycles = 6
+    monthly_deposit_money = 4100
+
+    time_ = AmountOfTime(month_, 'month')
+
+    # 不考虑复利的情况
+    moneys = get_interest(annual_interest_rates, time_, monthly_deposit_money)
+    print('收益 =', moneys)
+    print('本利和 =', month_ * monthly_deposit_money + moneys)
+
+    # 考虑复利的情况
+    moneys = get_interest_(annual_interest_rates, time_, monthly_deposit_money, cycles)
+    print('收益 =', moneys)
+    print('本利和 =', month_ * monthly_deposit_money + moneys)
+
+    # 哎！ 穷啊！
